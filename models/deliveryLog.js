496f56bcffc6f4c236fa4e62ba7e75403ff0c687
MantasEdine <youcefmantas3@gmail.com>
2025-08-20 18:19:40 -0700

    adding deliveryLog Schema

---
+++ models/deliveryLog.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const DeliveryLogSchema = new mongoose.Schema({
+    
+    deliveryGuy :  {
+       
+         type : mongoose.Schema.Types.ObjectId,
+         ref:"DeliveryGuy",
+         required : true
+    },
+    store : {
+         type : mongoose.Schema.Types.ObjectId,
+         ref : "Store",
+         required : true
+
+    },
+    product : {
+     
+        type : mongoose.Schema.Types.ObjectId,
+        ref:"Product",
+        required : true,
+
+         
+    },
+    quantity : {
+        type : Number,
+        required : true,
+        min: 0
+    },
+    DeliveredAt : {
+         type : Date,
+         default : Date.now
+
+    }
+      
+
+},{timestamps : true})
+
+const DeliveryLog = mongoose.model("DeliveryLog",DeliveryLogSchema)
+export default DeliveryLog
